9e9efdef3e73f3d45a1d830816aa29894de72c96
Tyler <u7542691@anu.edu.au>
2023-03-24 14:59:03 +1100

    lab 5 tyler

--- src/comp1110/ass2/gittest/Main.java
+++ src/comp1110/ass2/gittest/Main.java
@@ -1,7 +1,9 @@
 package comp1110.ass2.gittest;
 
 public class Main {
-    public static void main(String[] args) {
 
+
+    public static void main(String[] args) {
+        B b = new B();
     }
 }

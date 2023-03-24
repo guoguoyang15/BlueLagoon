11e389773155da7877e809eb3ab045f52634bc84
Tyler <u7542691@anu.edu.au>
2023-03-24 14:53:18 +1100

    lab5

--- src/comp1110/ass2/gittest/B.java
+++ src/comp1110/ass2/gittest/B.java
@@ -3,7 +3,7 @@ package comp1110.ass2.gittest;
 public class B {
     @Override
     public static void main(String[] args) {
-        System.out.println("sup");
+        System.out.println("hi");
     }
 }
 

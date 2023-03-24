b71ebf5a0c636bdf5a6eb5163eeb9eac5ebefc94
Tyler <u7542691@anu.edu.au>
2023-03-24 15:30:17 +1100

    lab 5

--- src/comp1110/ass2/gittest/Main.java
+++ src/comp1110/ass2/gittest/Main.java
@@ -2,6 +2,10 @@ package comp1110.ass2.gittest;
 
 public class Main {
     public static void main(String[] args) {
-        C c= new C();
+        A a = new A();
+
+        B b = new B();
+
+        C c = new C();
     }
 }

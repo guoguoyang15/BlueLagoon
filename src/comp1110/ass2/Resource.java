3d77809d179e7eabff7aaf0d8bef1b5431cc8dad
Zhou Linsheng <linsheng.zhou@anu.edu.au>
2023-03-27 14:13:28 +1100

    modify the resources as Enum type

---
+++ src/comp1110/ass2/Resource.java
@@ -0,0 +1,9 @@
+package comp1110.ass2;
+
+public enum Resource {
+    COCONUT,
+    BAMBOO,
+    WATER,
+    PRECIOUSSTONE,
+    STATUETTE;
+}
--- src/comp1110/ass2/Spot.java
+++ src/comp1110/ass2/Spot.java
@@ -3,10 +3,10 @@ package comp1110.ass2;
 public class Spot {
     int occupiedByPlayer;
     int spotType;
-    int resources;
+    Resource resources;
     int island;
     int settlerOrVillage;
-    Player[] mojority(int island){
+    public static Player[] mojority(int island){
 
         return null;
     }

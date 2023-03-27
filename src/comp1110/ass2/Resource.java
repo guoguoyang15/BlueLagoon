2a37636b5532947ab264883d2d4082fc5e339e54
Zhou Linsheng <linsheng.zhou@anu.edu.au>
2023-03-27 14:43:01 +1100

    modify some classes

--- src/comp1110/ass2/Resource.java
+++ src/comp1110/ass2/Resource.java
@@ -1,6 +1,7 @@
 package comp1110.ass2;
 
 public enum Resource {
+    NULL,//initialization,
     COCONUT,
     BAMBOO,
     WATER,
--- src/comp1110/ass2/Spot.java
+++ src/comp1110/ass2/Spot.java
@@ -1,11 +1,71 @@
 package comp1110.ass2;
 
 public class Spot {
-    int occupiedByPlayer;
-    int spotType;
-    Resource resources;
-    int island;
-    int settlerOrVillage;
+    public enum SpotType{
+        LAND,
+        SEA;
+    }
+    public enum SettlerOrVillage{
+        NULL,
+        SETTLER,
+        VILLAGE;
+    }
+    public int occupiedByPlayer;
+    public SpotType spotType;
+    public Resource resources;
+    public int island;//No. of island the spot belongs to
+    public SettlerOrVillage settlerOrVillage;
+
+    public Spot() {
+        this.occupiedByPlayer=100;//there are players 0~3, at first nobody occupies this spot
+        this.spotType=SpotType.SEA;//We can change it later, those are still not changed will be sea
+        this.resources=Resource.NULL;
+        this.island=100;//we change it later
+        this.settlerOrVillage=SettlerOrVillage.NULL;//At first nothing this spot
+    }
+
+    public int getOccupiedByPlayer() {
+        return occupiedByPlayer;
+    }
+
+    public void setOccupiedByPlayer(int occupiedByPlayer) {
+        this.occupiedByPlayer = occupiedByPlayer;
+    }
+
+    public SpotType getSpotType() {
+        return spotType;
+    }
+
+    public void setSpotType(SpotType spotType) {
+        this.spotType = spotType;
+    }
+
+    public Resource getResources() {
+        return resources;
+    }
+
+    public void setResources(Resource resources) {
+        this.resources = resources;
+    }
+
+    public int getIsland() {
+        return island;
+    }
+
+    public void setIsland(int island) {
+        this.island = island;
+    }
+
+    public SettlerOrVillage getSettlerOrVillage() {
+        return settlerOrVillage;
+    }
+
+    public void setSettlerOrVillage(SettlerOrVillage settlerOrVillage) {
+        this.settlerOrVillage = settlerOrVillage;
+    }
+
+
+
     public static Player[] mojority(int island){
 
         return null;

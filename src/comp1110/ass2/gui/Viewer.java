1c9cf13bce6f6ed0d14f0b48dc8c8e9e2827ff85
Zhang Zhining <u7580614@anu.edu.au>
2023-04-17 03:46:16 +1000

    add details related to task 5

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -35,6 +35,7 @@ public class Viewer extends Application {
      *
      * @param stateString a string representing a game state
      */
+    // FIXME Task 5
     void displayState(String stateString) {
         String[] parts = stateString.split(";");
         Polygon[] tiles = new Polygon[400];
